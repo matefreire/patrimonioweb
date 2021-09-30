bce352e4ccc0862852d21992a3f11f11c8580644
matefreire <matefreire@github.com>
2021-09-29 21:54:25 -0400

    Alteracao 21:54

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,2 +1,24 @@
 const db = require('./db');
-// const app = require
+
+// Criando tabela usuários
+const Usuario = db.sequelize.define('usuarios', {
+    codigo: {
+        type: Sequelize.INTEGER
+    }, 
+    nome: {
+        type: Sequelize.STRING
+    },
+    email: {
+        type: Sequelize.STRING
+    },
+    login: {
+        type: Sequelize.STRING
+    },
+    senha: {
+        type: Sequelize.STRING
+    }
+});
+
+Usuarios.sync();
+
+module.exports = Usuario;
--- models/db.js
+++ models/db.js
@@ -8,27 +8,6 @@ sequelize.authenticate().then(()=>{
     console.log('Erro ao se conectar ao banco. Erro: '+e);
 });
 
-// Criando tabela usuários
-const Usuarios = sequelize.define('usuarios', {
-    codigo: {
-        type: Sequelize.INTEGER
-    }, 
-    nome: {
-        type: Sequelize.STRING
-    },
-    email: {
-        type: Sequelize.STRING
-    },
-    login: {
-        type: Sequelize.STRING
-    },
-    senha: {
-        type: Sequelize.STRING
-    }
-});
-
-Usuarios.sync();
-
 module.exports = {
     Sequelize = Sequelize,
     sequelize = sequelize
